d0a62417de69517a346e2fdb522e66aa358ee576
luigi10072 <163684514+luigi10072@users.noreply.github.com>
2025-07-09 13:53:51 -0600

    Update database.php

--- database.php
+++ database.php
@@ -1,10 +1,12 @@
 <?php
 // database.php
 function conectarDB() {
-    $host = 'localhost'; // O la IP de tu servidor de base de datos
-    $db = 'pokemon';     // El nombre de la base de datos que creaste
-    $user = 'root';      // Tu usuario de MySQL
-    $pass = '';          // Tu contraseña de MySQL (vacía si no tienes)
+    $host = getenv('MYSQL_HOST') ?: 'mysql.railway.internal'; // O la IP de tu servidor de base de datos
+    $port = getenv('MYSQL_PORT') ?: '3306'
+    $db = getenv('MYSQL_DATABASE') ?: 'pokemon';     // El nombre de la base de datos que creaste
+    $user = getenv('MYSQL_USER') ?: 'root';      // Tu usuario de MySQL
+    $pass = getenv('MYSQL_PASSWORD') ?: 'IwSjZUqVxDIceOLuBBaPKzUPvfoCHlQv';          // Tu contraseña de MySQL (vacía si no tienes)
+    
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
@@ -18,7 +20,8 @@ function conectarDB() {
         $pdo = new PDO($dsn, $user, $pass, $options);
         return $pdo;
     } catch (PDOException $e) {
-        throw new PDOException($e->getMessage(), (int)$e->getCode());
+        error_log("Database connection error: " . $e->getMessage())
+        throw new Exception("Error al conectar con la base de datos. Por favor, inténtelo de nuevo más tarde.");
     }
 }
 ?>
